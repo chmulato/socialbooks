4eaf34fd9042eb0e1a26cb0f80422e0a59f50b68
chmulato <chmulato@gmail.com>
2017-01-24 14:04:15 -0200

    Adicionando o recurso Autor à API

--- src/main/java/com/algaworks/socialbooks/domain/Livro.java
+++ src/main/java/com/algaworks/socialbooks/domain/Livro.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -31,11 +33,14 @@ public class Livro {
 	@JsonInclude(Include.NON_NULL)
 	private String resumo;
 	
+	@JsonInclude(Include.NON_NULL)
 	@OneToMany(mappedBy = "livro")
 	private List<Comentario> comentarios;
 	
+	@ManyToOne
+	@JoinColumn(name = "AUTOR_ID")
 	@JsonInclude(Include.NON_NULL)
-	private String autor;
+	private Autor autor;
 
 	public Livro() {
 	}
@@ -92,11 +97,11 @@ public class Livro {
 		this.comentarios = comentarios;
 	}
 
-	public String getAutor() {
+	public Autor getAutor() {
 		return autor;
 	}
 
-	public void setAutor(String autor) {
+	public void setAutor(Autor autor) {
 		this.autor = autor;
 	}
 	
