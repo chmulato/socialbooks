cdda82167d2c5aee7ad416af1a76e2ef59c016cf
chmulato <chmulato@gmail.com>
2017-01-24 10:06:53 -0200

    Manipulando erros com @ExceptionHandler e @ControllerAdvice

--- src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -15,7 +15,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algaworks.socialbooks.domain.Livro;
 import com.algaworks.socialbooks.services.LivrosService;
-import com.algaworks.socialbooks.services.exceptions.LivroNaoEncontradoException;
 
 @RestController
 @RequestMapping("/livros")
@@ -32,41 +31,26 @@ public class LivrosResources {
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> salvar(@RequestBody Livro livro) {
 		livro = livrosService.salvar(livro);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{id}").buildAndExpand(livro.getId()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(livro.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> buscar(@PathVariable("id") Long id) {
-		Livro livro = null;
-		try {
-			livro = livrosService.buscar(id);
-		} catch (LivroNaoEncontradoException ex) {
-			// resposta para quando o recurso nao existe
-			return ResponseEntity.notFound().build();
-		}
+		Livro livro = livrosService.buscar(id);
 		return ResponseEntity.status(HttpStatus.OK).body(livro);
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> deletar(@PathVariable("id") Long id) {
-		try {
-			livrosService.deletar(id);
-		} catch (LivroNaoEncontradoException ex) {
-			return ResponseEntity.notFound().build();
-		}
+		livrosService.deletar(id);
 		return ResponseEntity.noContent().build();
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> atualizar(@RequestBody Livro livro, @PathVariable("id") Long id) {
-		try {
-			livro.setId(id);
-			livrosService.atualizar(livro);
-		} catch (LivroNaoEncontradoException ex) {
-			return ResponseEntity.notFound().build();
-		}
+		livro.setId(id);
+		livrosService.atualizar(livro);
 		return ResponseEntity.noContent().build();
 	}
 }
