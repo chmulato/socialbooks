b960b090d4de559c3d9e5df7b06751e97e6f1a7e
chmulato <chmulato@gmail.com>
2017-01-24 13:37:12 -0200

    Listando os comentários do recurso Livro.

--- src/main/java/com/algaworks/socialbooks/services/LivrosService.java
+++ src/main/java/com/algaworks/socialbooks/services/LivrosService.java
@@ -62,4 +62,9 @@ public class LivrosService {
 		comentario.setData(new Date());
 		return comentariosRepository.save(comentario);
 	}
+	
+	public List<Comentario> listarComentarios(Long livroId) {
+		Livro livro = buscar(livroId);
+		return livro.getComentarios();
+	}
 }
