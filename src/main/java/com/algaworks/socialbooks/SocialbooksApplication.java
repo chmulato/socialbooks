ac9c67392beaa0d581403db6323662dc59054004
chmulato <chmulato@gmail.com>
2017-01-25 16:45:38 -0200

    Renomeação da classe principal.

--- src/main/java/com/algaworks/socialbooks/SocialbooksApplication.java
+++ src/main/java/com/algaworks/socialbooks/SocialbooksApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SocialbooksapiApplication {
+public class SocialbooksApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SocialbooksapiApplication.class, args);
+		SpringApplication.run(SocialbooksApplication.class, args);
 	}
 }
